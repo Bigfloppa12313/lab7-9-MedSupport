23db01354f04f699f0dde6b06f3951b9a42a2c86
Andriy <248853939+Bigfloppa12313@users.noreply.github.com>
2025-12-12 14:01:30 +0200

    Added Entities to DAL

---
+++ Catalog/DAL/Entities/Class.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Catalog.DAL.Entities
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public string name { get; set; }
+        private string email { get; set; }
+        private string password { get; set; }
+        private string login { get; set; }
+    }
+    public class Admin : User
+    {
+    }
+    public class Employee : User
+    {
+        private int employeeId { get; set; }
+        public string position { get; set; }
+        public string department { get; set; }
+
+    }
+    public class MedicalStaff : User
+    {
+        private int doctorId { get; set; }
+        public string position { get; set; }
+    }
+}
